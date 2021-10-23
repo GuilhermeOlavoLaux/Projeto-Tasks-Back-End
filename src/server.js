77afd3eab275efd6291fb1369730c0e258768ab5
Guilherme Laux <guilherme.laux@compliancetotal.com.br>
2021-10-23 16:56:25 -0300

    adiciona cors

--- src/server.js
+++ src/server.js
@@ -2,12 +2,14 @@ const express = require('express')
 const mongoose = require('mongoose');
 const routes = require('./routes/tasksRoutes')
 const conectToDatabase = require('./dataBase')
+const cors = require('cors')
 require("dotenv").config()
 
 conectToDatabase()
 
 const app = express();
 const port = 3000;
+app.use(cors)
 app.use(express.json())
 app.use(routes)
 
